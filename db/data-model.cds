7a1f4a548aea50958ce5287988cfe839d1712839
Mohsin/918 <mohsinquantum@gmail.com>
2024-03-04 17:49:58 +0530

    get pending invoice list api

--- db/data-model.cds
+++ db/data-model.cds
@@ -1,7 +1,31 @@
 namespace my.bookshop;
 
-entity Books {
-  key ID : Integer;
-  title  : String;
-  stock  : Integer;
+entity GetPendingInvoiceList {
+  key PONumber                    : String;
+      PODate                      : String;
+      VendorCode                  : String;
+      VendorName                  : String;
+      PlantCode                   : String;
+      PlantName                   : String;
+      POTotalAmount               : String;
+      POTotalQuantity             : String;
+      MRNNumber                   : String;
+      VoucherNumber               : String;
+      MRNDate                     : String;
+      MRNQuantity                 : String;
+      MRNCurrency                 : String;
+      MRNQuantityReceived         : String;
+      MRNQuantityRejected         : String;
+      MRNQuantityReceivedLocation : String;
+      ASNQuantity                 : String;
+      InvoiceStatus               : String;
+      SendToAccDate               : String;
+      TillDatePurchaseVal         : String;
+      DedTds                      : String;
+      TotalDebit                  : String;
+      TotalCredit                 : String;
+      VoucherType                 : String;
+      AccCode                     : String;
+      AccDesc                     : String;
+      ReceiptDate                 : String;
 }
---
+++ srv/cat-service.js
@@ -0,0 +1,47 @@
+const cds = require('@sap/cds');
+const axios = require('axios');
+
+module.exports = (srv) => {
+
+    const { GetPendingInvoiceList } = srv.entities;
+
+    srv.on('READ', GetPendingInvoiceList, async (req) => {
+        const params = req._queryOptions;
+        const results = await getPendingInvoiceList(params);
+        if (!results) throw new Error('Unable to fetch Pending Invoice List.');
+        return results
+
+    });
+};
+
+async function getPendingInvoiceList(params) {
+    try {
+        const {
+            UnitCode, PoNum, MrnNumber, FromPOdate, ToPOdate,
+            FromMrndate, ToMrndate, Status
+        } = params;
+
+        const url = `https://imperialauto.co:84/IAIAPI.asmx/GetPendingInvoiceList?RequestBy='Manikandan'&UnitCode='${UnitCode}'&PoNum='${PoNum}'&MrnNumber='${MrnNumber}'&FromPOdate='${FromPOdate}'&ToPOdate='${ToPOdate}'&FromMrndate='${FromMrndate}'&ToMrndate='${ToMrndate}'&Status='${Status}'`;
+
+
+        const response = await axios({
+            method: 'get',
+            url: url,
+            headers: {
+                'Authorization': 'Bearer IncMpsaotdlKHYyyfGiVDg==',
+                'Content-Type': 'application/json'
+            },
+            data: {}
+        });
+
+        if (response.data && response.data.d) {
+            return JSON.parse(response.data.d);
+        } else {
+            console.error('Error parsing response:', response.data);
+            throw new Error('Error parsing the response from the API.');
+        }
+    } catch (error) {
+        console.error('Error in get Pending Invoice List API call:', error);
+        throw new Error('Unable to fetch Pending Invoice List.');
+    }
+}
